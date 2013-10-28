0d8ab511d12e710aa473384503c46fded57b59f4
David Cohen <david.a.cohen@linux.intel.com>
2013-10-28 11:12:33 -0700

    x86: intel-mid: add Merrifled platform code for mrst_max3110 driver
    This patch adds Merrifield platform code to enable serial console over
    mrst_max3110.
    
    Change-Id: I8c370eb2812cab15a026b98a6e3ef46f1de99208
    Signed-off-by: David Cohen <david.a.cohen@linux.intel.com>
    Signed-off-by: Fei Yang <fei.yang@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_max3111.c
+++ arch/x86/platform/intel-mid/device_libs/platform_max3111.c
@@ -10,19 +10,41 @@
  * of the License.
  */
 
+#include <linux/err.h>
 #include <linux/gpio.h>
+#include <linux/spi/intel_mid_ssp_spi.h>
 #include <linux/spi/spi.h>
 #include <asm/intel-mid.h>
+
+static struct intel_mid_ssp_spi_chip chip = {
+	.burst_size = DFLT_FIFO_BURST_SIZE,
+	.timeout = DFLT_TIMEOUT_VAL,
+};
 
 static void __init *max3111_platform_data(void *info)
 {
 	struct spi_board_info *spi_info = info;
-	int intr = get_gpio_by_name("max3111_int");
+	int intr;
 
 	spi_info->mode = SPI_MODE_0;
-	if (intr == -1)
-		return NULL;
-	spi_info->irq = intr + INTEL_MID_IRQ_OFFSET;
+
+	if (intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_TANGIER) {
+		spi_info->controller_data = &chip;
+
+/* FIXME: workaround for invalid bus number in IAFW .25 */
+#define FORCE_SPI_BUS_NUM	5
+
+		spi_info->bus_num = FORCE_SPI_BUS_NUM;
+	} else {
+		intr = get_gpio_by_name("max3111_int");
+		if (intr < 0) {
+			pr_err("%s: failed to get gpio 'max3111_int'\n",
+			       __func__);
+			return ERR_PTR(intr);
+		}
+		spi_info->irq = intr + INTEL_MID_IRQ_OFFSET;
+	}
+
 	return NULL;
 }
 
